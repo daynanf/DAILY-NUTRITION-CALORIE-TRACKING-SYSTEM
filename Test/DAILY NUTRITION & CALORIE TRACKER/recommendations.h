f98298b84b9f78c3e6530044c3f06b319b36e543
daynan f <dinuzeynu22@gmail.com>
2026-01-06 01:11:21 +0300

    build final version exe

--- Test/DAILY NUTRITION & CALORIE TRACKER/recommendations.h
+++
@@ -1,9 +0,0 @@
-#ifndef RECOMMENDATIONS_H
-#define RECOMMENDATIONS_H
-
-#include "models.h"
-
-void runMealRecommendations(const UserProfile& p);
-void runEndDaySummary(const UserProfile& p);
-
-#endif
